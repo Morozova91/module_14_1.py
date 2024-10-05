2d2690ae36d2239edd61a006ecd4d9ff737074c8
Юлия <118264321+Morozova91@users.noreply.github.com>
2024-10-05 20:36:58 +0300

    module_14_1.py

--- module_14_1.py
+++ module_14_1.py
@@ -22,7 +22,10 @@ for i in range(0, 9, 2):
         cursor.execute("UPDATE Users SET balance = ? WHERE id = ?", (500, i + 1))
 for i in range(1, 11, 3):
     cursor.execute("DELETE FROM Users WHERE id = ?", (i,))
-cursor.execute('SELECT username, age FROM Users WHERE age != 60')
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != 60')
 users = cursor.fetchall()
+for user in users:
+    print(f"Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}")
+
 connection.commit()
 connection.close()
